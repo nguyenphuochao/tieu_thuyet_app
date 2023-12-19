a93027cd4a1d6bdfae60e57cc1b06b0a3195c936
Hảo Nguyễn <nguyenphuochao123@gmail.com>
2023-12-19 10:26:43 +0700

    m

--- screens/Home.js
+++ screens/Home.js
@@ -1,49 +1,60 @@
 import { View, Text, Image, ScrollView, TextInput } from "react-native";
 import { fontSize } from "../contants";
 import StoryList from "./StoryList/StoryList";
-import { useState } from "react";
-function Home() {
-    const [newStory, setNewStory] = useState([
-        {
-            id: 1,
-            name: 'Tình Sắc Khó Cưỡng',
-            image: 'https://tieuthuyet.vn/images/2023/12/sac-tinh-kho-cuong.jpeg'
-        },
-        {
-            id: 2,
-            name: 'Mộng Tình',
-            image: 'https://tieuthuyet.vn/images/2023/12/mong-tinh.jpeg'
-        },
-        {
-            id: 3,
-            name: 'Vụng Trộm Không Thể Dấu',
-            image: 'https://tieuthuyet.vn/images/2023/12/vung-trom-khong-the-giau.jpeg'
-        }
-    ]);
-    const [reviewStory, setReviewStory] = useState([
-        {
-            id: 1,
-            name: 'Đế Tôn',
-            image: 'https://tieuthuyet.vn/images/2023/10/de-ton.jpeg'
-        },
-        {
-            id: 2,
-            name: 'Mộng Tình',
-            image: 'https://tieuthuyet.vn/images/2023/10/ky-duyen-tuong-ngo.jpeg'
-        },
-        {
-            id: 3,
-            name: 'Thần Ma Thiên Tôn',
-            image: 'https://tieuthuyet.vn/images/2023/11/than-ma-thien-ton.jpeg'
-        }
-    ]);
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+function Home(props) {
+    const [isLoading, setLoading] = useState(true);
+    const [newStory, setNewStory] = useState([]);
+    const [reviewStory, setReviewStory] = useState([]);
+    const [fullStory, setFullStory] = useState([]);
+    const [truyenVui, setTruyenVui] = useState([]);
+    useEffect(() => {
+        // Gọi API để lấy danh sách truyện mới
+        axios.get('https://tieuthuyet.vn/api/truyen-moi')
+            .then(response => {
+                // Xử lý dữ liệu từ API
+                setNewStory(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching product list:', error);
+            });
+        // Gọi API để lấy danh sách truyện đọc nhiều
+        axios.get('https://tieuthuyet.vn/api/truyen-doc-nhieu')
+            .then(response => {
+                // Xử lý dữ liệu từ API
+                setReviewStory(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching product list:', error);
+            });
+        // Gọi API để lấy danh sách truyện Full
+        axios.get('https://tieuthuyet.vn/api/truyen-full')
+            .then(response => {
+                // Xử lý dữ liệu từ API
+                setFullStory(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching product list:', error);
+            });
+        // Gọi API để lấy danh sách truyện vui
+        axios.get('https://tieuthuyet.vn/api/the-loai/47')
+        .then(response => {
+            // Xử lý dữ liệu từ API
+            setTruyenVui(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching product list:', error);
+        });
+    }, []); // useEffect sẽ chạy một lần khi component được mount
     return <ScrollView>
         <View style={{ paddingLeft: 10, paddingRight: 10, paddingTop: 4, backgroundColor: 'red', height: 200 }}>
             <TextInput style={{ backgroundColor: '#c7c4bb', borderRadius: 20, height: 45 }} placeholder="Nhập nội dung tìm kiếm" />
         </View>
         <StoryList heading="Tiểu Thuyết Mới Nhất" storyList={newStory} />
         <StoryList heading="Ai Cũng Thích Đọc" storyList={reviewStory} />
-        <StoryList heading="Truyện FULL" />
+        <StoryList heading="Truyện Full" storyList={fullStory} />
+        <StoryList heading="Truyện Vui" storyList={truyenVui} />
     </ScrollView>
 }
 export default Home;
--- screens/StoryList/StoryItem.js
+++ screens/StoryList/StoryItem.js
@@ -2,9 +2,9 @@ import { View, Text, Image } from "react-native";
 import { fontSize } from "../../contants";
 function StotyItem(props) {
     const { name, image } = props;
-    return <View style={{ width: '33.33%' }}>
-        <Image style={{ width: 105, height: 150, borderRadius: 10 }} source={{ uri: image }}></Image>
-        <Text>{name}</Text>
+    return <View style={{ width: '33.33%', marginBottom: 10 }}>
+        <Image style={{ width: 105, height: 150, borderRadius: 10 }} source={{ uri: 'https://tieuthuyet.vn/' + image }}></Image>
+        <Text style={{marginTop: 5}} numberOfLines={1}>{name}</Text>
     </View>
 }
 export default StotyItem;
